d016415641a92f6a82d5d14ec1cfc8fe6291ef84
ValeriyMakarov <misteranon.com@gmail.com>
2024-01-25 16:15:32 +0200

    optimisation and changes

--- source/reader.py
+++ source/reader.py
@@ -1,17 +1,17 @@
-from pdfminer.high_level import extract_text, extract_pages
-import re
 import fitz
 from pyzbar.pyzbar import decode
 from PIL import Image
 
 
-def read_barcodes(pdf_path: str) -> list[dict]:
-    """Reads the first page of PDF document and returns the list of
-    dictionaries with info about decoded barcodes on the page"""
+def read_barcodes(page: fitz.Page) -> list[dict]:
+    """
+    Reads the first page of PDF document and returns the list of
+    dictionaries with info about decoded barcodes on the page
+    :param page: fitz.Page
+    :return: list[dict]
+    """
 
-    with fitz.open(pdf_path) as pdf_file:
-        page = pdf_file.load_page(0)
-        pixel_map = page.get_pixmap()
+    pixel_map = page.get_pixmap()
     page_image = Image.frombytes(
         "RGB", [pixel_map.width, pixel_map.height], pixel_map.samples
     )
@@ -20,55 +20,64 @@ def read_barcodes(pdf_path: str) -> list[dict]:
         key=lambda x: (x.rect.left, x.rect.top)
     )
 
-    return [
+    decoded_list = [
         {
             'barcode_type': barcode.type,
             'barcode_data': barcode.data.decode('utf-8')
         }
         for barcode in decoded
     ]
+    return decoded_list
 
 
-def get_value_regexp(regexp: str, from_str: str):
-    matches = re.search(regexp, from_str, re.DOTALL)
-    return matches.group(1) if len(matches.groups()) else None
+def read_data_from_page(page: fitz.Page) -> dict:
+    """
+    Reads all data from page
+    :param page: fitz.Page
+    :return: dict
+    """
 
+    text_data_list = [i for i in page.get_text().split('\n') if i.strip()]
+    barcodes = read_barcodes(page)
 
-def read_inner_data_from_pdf(pdf: str) -> dict:
-    """Reads data from PDF file"""
-
-    text = extract_text(pdf)
-    barcodes = read_barcodes(pdf)
-
-    data_dict = {
-        'LABEL': get_value_regexp(r'(^(\w+ *)*).*', text),
-        'BARCODE': barcodes[0],
-        'PN': get_value_regexp(r'PN: (\w+).*DESCRIPTION:', text),
-        'DESCRIPTION': get_value_regexp(r'DESCRIPTION: (\w+).*LOCATION:', text),
-        'LOCATION': get_value_regexp(r'LOCATION: (\d+).*RECEIVER#:', text),
-        'RECEIVER#': get_value_regexp(r'RECEIVER#: (\d+).*EXP DATE:', text),
-        'EXP DATE': get_value_regexp(r'EXP DATE: ((\d+\.?){3}).*CERT SOURCE:', text),
-        'CERT SOURCE': get_value_regexp(r'CERT SOURCE: (\S+).*SN:', text),
-        'SN': get_value_regexp(r'SN: (\d+).*CONDITION:', text),
-        'CONDITION': get_value_regexp(r'CONDITION: (\w+).*UOM:', text),
-        'UOM': get_value_regexp(r'UOM: (\w+).*PO:', text),
-        'PO': get_value_regexp(r'PO: (\w+).*REC.DATE:', text),
-        'REC.DATE': get_value_regexp(r'REC.DATE: ((\d+\.?){3}).*MFG:', text),
-        'MFG': get_value_regexp(r'MFG: (\w+).*BATCH# :', text),
-        'BATCH#': get_value_regexp(r'BATCH# : (\d+).*REMARK:', text),
-        'REMARK': get_value_regexp(r'REMARK:(\S+)*.*TAGGED BY:', text),
-        'TARGET BY': {
-            'BARCODE': barcodes[1],
-            'Qty': get_value_regexp(r'Qty: (\S+).*DOM:', text),
+    page_data_dict = {
+        'LABEL': text_data_list[0],
+        'BARCODE': barcodes[0] if len(barcodes) == 2 else None,
+        'TAGGED BY': {
+            'BARCODE': barcodes[1] if len(barcodes) == 2 else None,
+            'Qty': None
         },
-        'DOM': get_value_regexp(r'DOM: ((\d+\.?){3}).*LOT# :', text),
-        'LOT#': get_value_regexp(r'LOT# : (\w+).*NOTES:', text),
-        'NOTES': get_value_regexp(r'NOTES:\s+((\S+ *)*).*', text),
+        'NOTES': text_data_list[-1]
     }
+    for item in text_data_list[1:-1]:
+        if 'TAGGED BY' in item or 'NOTES' in item:
+            continue
+        elif 'Qty' in item:
+            page_data_dict['TAGGED BY']['Qty'] = item.split(':')[1].strip()
+        else:
+            items = item.split(':')
+            page_data_dict[items[0]] = items[1] if len(items) == 2 else None
 
-    return data_dict
+    return page_data_dict
+
+
+def read_data_from_pdf(pdf_path: str) -> dict[dict]:
+    """
+    Reads all data from PDF file by page
+    :param pdf_path: str
+    :return: dict[dict]
+    """
+
+    pdf_data_dict = {}
+
+    with fitz.open(pdf_path) as pdf_file:
+        for page_number in range(pdf_file.page_count):
+            page = pdf_file.load_page(page_number)
+            pdf_data_dict[page_number] = read_data_from_page(page)
+
+    return pdf_data_dict
 
 
 if __name__ == "__main__":
-    print(read_inner_data_from_pdf(r'task_data\test_task.pdf'))
+    print(read_data_from_pdf(r'task_data\test_task.pdf'))
 
